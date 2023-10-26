7f523f3c07abff2a221d2fbba519dc7df8b98865
PinkRabbit <pink.rabbit.lzb@gmail.com>
2023-10-26 10:59:15 +0800

    perf: 增加调度器，将更新bw工具任务设为定时任务

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 
-from utils import get_bw, run_commands
+from utils import get_bw, run_commands, update_bw
 
 bw = get_bw()
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+# 创建一个后台调度器
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+
+# 定义每日凌晨执行的任务函数
+def daily_task():
+    # 在这里执行您想要每日凌晨执行的任务
+    update_bw()
+
+
+# 使用 CronTrigger 触发器安排任务，每日凌晨执行
+trigger = CronTrigger(hour=0, minute=0)
+scheduler.add_job(daily_task, trigger)
 
 
 @app.get("/")
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-__all__ = ["response_temp", "run_commands", "get_os", "get_bw"]
+__all__ = ["response_temp", "run_commands", "get_os", "get_bw", "update_bw"]
 
 import os
 import platform
@@ -17,11 +17,11 @@ class DownloadError(Exception):
 
 
 # 响应信息模板
-def response_temp(code: int = 200, message: str or dict = None, payload: any = None, **kwargs):
+def response_temp(code: int = 200, message: str or dict = None, payload: any = None, **kwargs) -> dict:
     return {"Code": code, "Message": message, "Payload": payload, **kwargs}
 
 
-def download_bw_tool(bw_path: str, bw_name: str):
+def download_bw_tool(bw_path: str, bw_name: str) -> str:
     _bw = os.path.join(bw_path, bw_name)
     system = bw_name.split('_')[1].replace('.exe', '')
     url = f"https://vault.bitwarden.com/download/?app=cli&platform={system}"
@@ -49,13 +49,13 @@ def download_bw_tool(bw_path: str, bw_name: str):
                     break
 
         # 删除ZIP文件
-        os.remove(zip_file_path)
+        # os.remove(zip_file_path)
         return _bw
     else:
         raise DownloadError(f'BW 工具下载失败, 原错误：{response.content.decode("utf-8")}')
 
 
-def run_commands(commands: str):
+def run_commands(commands: str) -> dict:
     process = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
                                universal_newlines=True)
     stdout, stderr = process.communicate()
@@ -65,7 +65,7 @@ def run_commands(commands: str):
         return response_temp(message=stdout)
 
 
-def get_os():
+def get_os() -> str:
     # 获取操作系统名称
     os_name = _os_name.lower() if (_os_name := platform.system()) != "Darwin" else "macos"
     if os_name != "windows" and os_name != "linux" and os_name != "macos":
@@ -74,7 +74,7 @@ def get_os():
         return os_name
 
 
-def get_bw():
+def get_bw() -> str:
     os_name = get_os()
     # 获取 BW 工具
     return _bw_whole_path if os.path.exists(
@@ -83,3 +83,42 @@ def get_bw():
             (_bw_name := (f"bw_windows.exe" if os_name == "windows" else f"bw_{os_name}"))
         )
     ) else download_bw_tool(bw_name=_bw_name, bw_path=_bw_path)
+
+
+# 定义一个函数来从文件名中提取日期信息
+def extract_date(filename) -> str:
+    parts = filename.split('-')
+    for part in parts:
+        if part.count('.') == 2 and all(char.isdigit() or char == '.' for char in part):
+            return part
+
+    # 如果没有有效日期信息，返回文件名本身
+    return filename
+
+
+def update_bw() -> None:
+    os_name = get_os()
+    if os.path.exists(
+            _bw_whole_path := os.path.join(
+                (_bw_path := os.path.dirname(os.path.abspath(__file__))),
+                (_bw_name := (f"bw_windows.exe" if os_name == "windows" else f"bw_{os_name}"))
+            )
+    ):
+        matching_files = [
+            filename for filename in os.listdir(os.path.dirname(os.path.abspath(__file__)))
+            if filename.startswith(f"bw-{os_name}")
+        ]
+        latest_file = max(matching_files, key=extract_date)
+        url = f"https://vault.bitwarden.com/download/?app=cli&platform={os_name}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            # 获取文件名
+            content_disposition = response.headers.get('Content-Disposition')
+            if content_disposition:
+                file_name = content_disposition.split('filename=')[1]
+            else:
+                file_name = os.path.basename(url)
+            if latest_file != file_name:
+                os.remove(os.path.join(_bw_path, latest_file))
+                os.remove(_bw_whole_path)
+                _bw = download_bw_tool(bw_name=_bw_name, bw_path=_bw_path)
